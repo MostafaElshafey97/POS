8388b798ba7c6126d44875ef1c5a09b1568eec5a
mostafa <elshafey1993@gmail.com>
2021-03-10 21:32:26 -0800

    last update

---
+++ resources/lang/en/site.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+    'dashboard' => 'General',
+    'logout' =>'logout ',
+
+    'add' => 'add',
+    'create' => 'create',
+    'read' => 'show',
+    'edit' => 'edit',
+    'update' => 'update',
+    'delete' => 'حذف',
+    'search' => 'بحث',
+    'show' => 'عرض',
+    'loading' => 'جاري التحميل',
+    'print' => 'print',
+
+    'confirm_delete' => 'تاكيد الحذف',
+    'yes' => 'نعم',
+    'no' => 'لا',
+
+    'login' => 'تسجيل الدخول',
+    'remember_me' => 'تذكرني',
+    'password' => 'كلمه المرور',
+    'password_confirmation' => 'تاكيد كلمه المرور',
+
+    'added_successfully' => 'تم اضافه البيانات بنجاح',
+    'updated_successfully' => 'تم تعديل البيانات بنجاح',
+    'deleted_successfully' => 'تم حذف البيانات بنجاح',
+
+    'no_data_found' => 'للاسف لا يوجد اي سجلات',
+    'no_records' => 'للاسف لا يوجد اي سجلات',
+
+    'clients' => 'clients',
+    'client_name' => 'client-name ',
+    'phone' => 'phone',
+    'address' => 'address',
+    'previous_orders' => 'الطلبات السابقه',
+    'orders' => 'الطلبات',
+    'add_order' => 'اضف طلب',
+    'edit_order' => 'تعديل طلب',
+
+    'users' => 'المشرفين',
+    'first_name' => 'الاسم الاول',
+    'last_name' => 'الاسم الاخير',
+    'email' => 'البريد الاكتروني',
+    'image' => 'صوره',
+    'action' => 'اكشن',
+
+    'permissions' => 'الصلاحيات',
+
+    'categories' => 'الاقسام',
+    'all_categories' => 'كل الاقسام',
+    'name' => 'الاسم',
+    'description' => 'الوصف',
+    'products_count' => 'عدد الامنتجات',
+    'related_products' => 'المنتجات المرتبطه',
+    'category' => 'القسم',
+    'show_products' => 'عرض المنتجات',
+    'created_at' => 'تم اضافته',
+
+    'products' => 'المنتجات',
+    'product' => 'المنتج',
+    'quantity' => 'الكميه',
+    'total' => 'المجموع',
+    'purchase_price' => 'سعر الشراء',
+    'price' => 'السعر',
+    'sale_price' => 'سعر البيع',
+    'stock' => 'المخزن',
+    'profit_percent' => 'المكسب',
+
+    'ar' => [
+        'name' => 'الاسم بالغه العربيه',
+        'description' => 'الوصف بالغه العربيه',
+    ],
+
+    'en' => [
+        'name' => 'الاسم بالغه الانجليزيه',
+        'description' => 'الوصف بالغه الانجليزيه',
+    ],
+
+];
